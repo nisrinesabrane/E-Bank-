b2fe13764cc35cebf3cacb5b96c3a228f597235b
nisrinesabrane <152791686+nisrinesabrane@users.noreply.github.com>
2025-01-19 17:46:25 +0100

    Update OperationType.java

--- src/main/java/org/sid/ebankingbackend/enums/OperationType.java
+++ src/main/java/org/sid/ebankingbackend/enums/OperationType.java
@@ -1,5 +1,12 @@
 package org.sid.ebankingbackend.enums;
 
+/**
+ * Enum representing the type of operations that can be performed on a bank account.
+ * 
+ * - DEBIT: Represents an operation that subtracts money from the account.
+ * - CREDIT: Represents an operation that adds money to the account.
+ */
 public enum OperationType {
-    DEBIT, CREDIT
+    DEBIT,  // Subtract money from the account
+    CREDIT  // Add money to the account
 }
