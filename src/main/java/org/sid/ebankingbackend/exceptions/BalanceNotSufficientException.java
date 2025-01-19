b6c4b444b3eaabb4c5ef4a69285e9ca34f547e60
nisrinesabrane <152791686+nisrinesabrane@users.noreply.github.com>
2025-01-19 17:47:11 +0100

    Update BalanceNotSufficientException.java

--- src/main/java/org/sid/ebankingbackend/exceptions/BalanceNotSufficientException.java
+++ src/main/java/org/sid/ebankingbackend/exceptions/BalanceNotSufficientException.java
@@ -1,6 +1,15 @@
 package org.sid.ebankingbackend.exceptions;
 
+/**
+ * Exception thrown when a bank account has insufficient balance to complete a transaction.
+ */
 public class BalanceNotSufficientException extends Exception {
+
+    /**
+     * Constructs a new BalanceNotSufficientException with the specified detail message.
+     * 
+     * @param message The detail message, typically describing the issue.
+     */
     public BalanceNotSufficientException(String message) {
         super(message);
     }
