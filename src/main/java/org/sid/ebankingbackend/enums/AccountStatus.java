15c18e5a4b5df0c2231cfa471b329f7f43c1df6f
nisrinesabrane <152791686+nisrinesabrane@users.noreply.github.com>
2025-01-19 17:45:51 +0100

    Update AccountStatus.java

--- src/main/java/org/sid/ebankingbackend/enums/AccountStatus.java
+++ src/main/java/org/sid/ebankingbackend/enums/AccountStatus.java
@@ -1,5 +1,14 @@
 package org.sid.ebankingbackend.enums;
 
+/**
+ * Enum representing the different statuses of a bank account.
+ * 
+ * - CREATED: When the account is created but not yet active.
+ * - ACTIVATED: When the account is active and operational.
+ * - SUSPENDED: When the account is temporarily disabled.
+ */
 public enum AccountStatus {
-    CREATED, ACTIVATED, SUSPENDED
+    CREATED, // Initial state of the account
+    ACTIVATED, // Account is operational
+    SUSPENDED // Account is disabled
 }
